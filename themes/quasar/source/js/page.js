0c2df6416d64d691401da422dbdf7753d5f494f2
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2016-05-25 18:44:47 +0300

    feat: Tweaks for Preview

--- themes/quasar/source/js/page.js
+++ themes/quasar/source/js/page.js
@@ -1,13 +1,16 @@
 $(function() {
 
-  var each = [].forEach;
-  var main = document.getElementById('main');
-  var doc = document.documentElement;
-  var body = document.body;
-  var header = document.getElementById('header');
-  var menu = document.querySelector('.sidebar');
-  var content = document.querySelector('.content');
-  var toTop = document.getElementById('toTop');
+  var
+    each = [].forEach,
+    main = document.getElementById('main'),
+    doc = document.documentElement,
+    body = document.body,
+    header = document.getElementById('header'),
+    menu = document.querySelector('.sidebar'),
+    content = document.querySelector('.content'),
+    toTop = document.getElementById('toTop'),
+    scrollSpeed = 400
+    ;
 
   var mobileBar = document.getElementById('mobile-bar');
   var menuButton = mobileBar.querySelector('.menu-button');
@@ -63,9 +66,6 @@ $(function() {
         menu.classList.remove('open');
         setActive(e.target);
         animating = true;
-        setTimeout(function() {
-          animating = false;
-        }, 400);
       }
     }, true);
 
@@ -74,8 +74,13 @@ $(function() {
 
     // init smooth scroll
     smoothScroll.init({
-      speed: 400,
-      offset: window.innerWidth > 720 ? 100 : 15
+      speed: scrollSpeed,
+      offset: window.innerWidth > 720 ? 100 : 15,
+      callback: function() {
+        setTimeout(function() {
+          animating = false;
+        }, 100);
+      }
     });
   }
 
@@ -93,7 +98,7 @@ $(function() {
     for (var i = 0; i < allLinks.length; i++) {
       var link = allLinks[i];
 
-      if (link.offsetTop > top) {
+      if (link.offsetTop >= top + headerHeight + 20) {
         if (!last) {
           last = link;
         }
@@ -162,9 +167,12 @@ $(function() {
       currentActive.classList.add('active');
 
       if (window.themePreview) {
-        var link = $('.content a[data-scroll][href="#' + id + '"]').next();
+        var
+          demoTarget = $(currentActive).attr('href').substring(1),
+          link = $('.content a[data-scroll][href="#' + demoTarget + '"]').next()
+          ;
 
-        window.themePreview.selectPage(link ? link.data('page') : '');
+        window.themePreview.selectPage(link ? link.data('demo-page') : '');
       }
     }
   }
@@ -253,10 +261,13 @@ $(function() {
     iframes = $('#preview iframe'),
     contentNode = $('#main > .content'),
     selectedTheme = $.cookie('theme') || 'android',
-    demoPoints = $('#main .content .demo-point')
+    demoPoints = $('#main .content input[data-demo-page]'),
+    currentPage,
+    // eslint-disable-next-line
+    isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))
     ;
 
-  if (themePicker.length === 0 || demoPoints.length === 0) {
+  if (isMobile || themePicker.length === 0 || demoPoints.length === 0) {
     return;
   }
 
@@ -268,21 +279,27 @@ $(function() {
     show: function(page) {
       previewNode.css('display', 'block');
       contentNode.addClass('with-previewer');
-      this.selectPage(page);
+      currentPage = page;
+      this.selectTheme(selectedTheme);
     },
     hide: function() {
       previewNode.css('display', 'none');
       contentNode.removeClass('with-previewer');
     },
     selectPage: function(page) {
+      currentPage = page;
+
       iframes.each(function() {
         var
           $this = $(this),
-          platform = $(this).attr('id').split('-')[0]
+          theme = $(this).attr('id').split('-')[0]
           ;
 
-        if (page) {
-          $this.attr('src', '/preview/' + platform + '.html#/' + page);
+        if (selectedTheme !== theme) {
+          return;
+        }
+        else if (page) {
+          $this.attr('src', '/preview/' + theme + '.html#/' + page);
         }
         else {
           $this.attr('src', '');
@@ -297,9 +314,11 @@ $(function() {
       $('#' + theme + '-preview').css('display', 'block');
 
       $.cookie('theme', theme);
+      selectedTheme = theme;
+
+      this.selectPage(currentPage);
     }
   };
 
-  window.themePreview.selectTheme(selectedTheme);
   window.themePreview.show();
 });
