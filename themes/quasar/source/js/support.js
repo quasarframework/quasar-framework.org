99141434eaef57e77e700ef664b28ed7fb2e5358
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2016-07-16 07:14:28 +0300

    feat: Update support page

--- themes/quasar/source/js/support.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('a.support-idea').click(function() {
-    var win = window.open('https://github.com/rstoenescu/quasar-framework/issues', '_blank');
-
-    if (win) {
-      win.focus();
-    }
-  });
-});
