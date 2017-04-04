933a07015af9dac43973f3d290507fe1c95ed8d4
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2017-04-04 12:22:44 +0800

    Replace jQuery with WebAPI and axios equivalents

--- themes/quasar/source/js/stylus-variables.js
+++ themes/quasar/source/js/stylus-variables.js
@@ -1,42 +1,12 @@
-$(function() {
-  var
-    variables = [],
-    input = $('#search-variable'),
-    table = $('#search-results')
-    ;
-
-  $.getJSON('/stylus-variables.json', function(data) {
-    Object.keys(data).forEach(function(property) {
-      variables.push([
-        property,
-        data[property].mat || '<em>Not Applicable</em>',
-        data[property].ios || '<em>Not Applicable</em>'
-      ]);
-    });
-
-    input
-      .removeAttr('disabled')
-      .attr('placeholder', 'Filter Stylus variable names')
-      .on('input', debounce(function() {
-        var token = $(this).val().trim();
-
-        if (token.length === 0) {
-          fillTable(variables);
-          return;
-        }
-
-        fillTable(variables.filter(function(item) {
-          return item[0].indexOf(token) > -1;
-        }));
-      }, 500));
-
-    fillTable(variables);
-  });
+(function() {
+  var variables = [];
+  var input = document.getElementById('search-variable');
+  var table = document.getElementById('search-results');
 
   function fillTable(rows) {
-    table.html(rows.map(function(row) {
+    table.innerHTML = rows.map(function(row) {
       return '<tr><td>' + row[0] + '</td><td>' + row[1] + '</td><td>' + row[2] + '</td></tr>';
-    }).join(''));
+    }).join('');
   }
 
   function debounce(func, wait) {
@@ -69,4 +39,34 @@ $(function() {
       return result;
     };
   }
-});
+
+  axios
+    .get('/stylus-variables.json')
+    .then(function (response) {
+      Object.keys(response.data).forEach(function (property) {
+        variables.push([
+          property,
+          data[property].mat || '<em>Not Applicable</em>',
+          data[property].ios || '<em>Not Applicable</em>'
+        ]);
+      });
+
+      input
+        .removeAttribute('disabled')
+        .setAttribute('placeholder', 'Filter Stylus variable names')
+        .addEventListener('input', debounce(function() {
+          var token = $(this).val().trim();
+
+          if (token.length === 0) {
+            fillTable(variables);
+            return;
+          }
+
+          fillTable(variables.filter(function(item) {
+            return item[0].indexOf(token) > -1;
+          }));
+        }, 500));
+
+      fillTable(variables);
+    });
+}());
